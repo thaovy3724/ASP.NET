f2e354e493d2d4398eee895a077a306f39a69d09
thaovy3724 <thaovy3724@gmail.com>
2026-01-24 10:08:12 +0700

    update template

---
+++ StoreApp/StoreApp.Api/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}
