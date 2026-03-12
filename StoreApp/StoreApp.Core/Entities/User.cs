a8d5bd0b393d2062a5ca46f858a4b9ec74476f96
thaovy3724 <thaovy3724@gmail.com>
2026-03-12 07:36:15 +0700

    verify email

--- StoreApp/StoreApp.Core/Entities/User.cs
+++ StoreApp/StoreApp.Core/Entities/User.cs
@@ -13,21 +13,12 @@ namespace StoreApp.Core.Entities
         public DateTime CreatedAt { get; private set; } = createdAt;
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiryTime { get; set; }
-        public bool IsActive { get; set; } = false;
-        // BẮT BUỘC: Thêm constructor trống để EF Core có thể khởi tạo đối tượng khi lấy dữ liệu từ DB
-        //private User() : this(default!, default!, default!, default!, default!) { }
-
         public void Update(string username, string fullName, string phone, Role role)
         {
             Username = username;
             FullName = fullName;
             Phone = phone;
             Role = role;
-        }
-
-        public void Activate()
-        {
-            IsActive = true;
         }
     }
 }
